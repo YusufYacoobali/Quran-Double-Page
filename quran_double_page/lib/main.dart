e0749a34c98815675362274e8f6b9b842375b225
Yusuf Yacoobali <yyacoobali@gmail.com>
2024-05-25 11:58:01 +0100

    getting there

--- quran_double_page/lib/main.dart
+++ quran_double_page/lib/main.dart
@@ -28,47 +28,158 @@ class MyPDFViewer extends StatefulWidget {
 }
 
 class _MyPDFViewerState extends State<MyPDFViewer> {
-  String? pdfPath;
+  String? portraitPdfPath;
+  String? landscapePdfPath;
 
   @override
   void initState() {
     super.initState();
-    loadPDFFromAsset('assets/quran_source_v.pdf');
+    loadPDFFromAsset('assets/quran_source_v.pdf', isLandscape: false);
+    loadPDFFromAsset('assets/quran_source_double_spaced.pdf',
+        isLandscape: true);
   }
 
-  Future<void> loadPDFFromAsset(String assetPath) async {
+  Future<void> loadPDFFromAsset(String assetPath,
+      {required bool isLandscape}) async {
     final ByteData data = await rootBundle.load(assetPath);
     final Directory tempDir = await getTemporaryDirectory();
-    final File tempFile = File('${tempDir.path}/quran_source_v.pdf');
+    final File tempFile = File(
+        '${tempDir.path}/${isLandscape ? 'quran_source_double_spaced.pdf' : 'quran_source_v.pdf'}');
     await tempFile.writeAsBytes(data.buffer.asUint8List(), flush: true);
     setState(() {
-      pdfPath = tempFile.path;
-      if (pdfPath != null) {
-        print('PDF loaded successfully: $pdfPath');
+      if (isLandscape) {
+        landscapePdfPath = tempFile.path;
       } else {
-        print('Error loading PDF');
+        portraitPdfPath = tempFile.path;
       }
+      print('PDF loaded successfully: ${tempFile.path}');
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: pdfPath != null
-          ? PDFView(
-              filePath: pdfPath!,
-              swipeHorizontal: true,
-              fitPolicy: FitPolicy.BOTH,
-              onError: (error) {
-                print('PDF loading error: $error');
-              },
-              onViewCreated: (PDFViewController controller) {
-                print('PDF loading successful!');
-              },
-            )
-          : const Center(
-              child: CircularProgressIndicator(),
-            ),
+      body: OrientationBuilder(
+        builder: (context, orientation) {
+          final String? pdfPath = orientation == Orientation.portrait
+              ? portraitPdfPath
+              : landscapePdfPath;
+
+          // Print statement to indicate which PDF is being displayed
+          if (orientation == Orientation.portrait) {
+            print('Displaying portrait PDF: $portraitPdfPath');
+          } else {
+            print('Displaying landscape PDF: $landscapePdfPath');
+          }
+
+          return pdfPath != null
+              ? PDFView(
+                  filePath: pdfPath!,
+                  swipeHorizontal: true,
+                  //fitPolicy: FitPolicy.BOTH,
+                  onError: (error) {
+                    print('PDF loading error: $error');
+                  },
+                  onPageError: (page, error) {
+                    print('Error on page $page: $error');
+                  },
+                  onViewCreated: (PDFViewController controller) {
+                    print('PDF loading successful!');
+                  },
+                )
+              : const Center(
+                  child: CircularProgressIndicator(),
+                );
+        },
+      ),
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import 'package:flutter/material.dart';
+// import 'package:flutter_pdfview/flutter_pdfview.dart';
+// import 'package:flutter/services.dart';
+// import 'package:path_provider/path_provider.dart';
+// import 'dart:io';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'PDF Viewer Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//         visualDensity: VisualDensity.adaptivePlatformDensity,
+//       ),
+//       home: MyPDFViewer(),
+//     );
+//   }
+// }
+
+// class MyPDFViewer extends StatefulWidget {
+//   @override
+//   _MyPDFViewerState createState() => _MyPDFViewerState();
+// }
+
+// class _MyPDFViewerState extends State<MyPDFViewer> {
+//   String? pdfPath;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     loadPDFFromAsset('assets/quran_source_v.pdf');
+//   }
+
+//   Future<void> loadPDFFromAsset(String assetPath) async {
+//     final ByteData data = await rootBundle.load(assetPath);
+//     final Directory tempDir = await getTemporaryDirectory();
+//     final File tempFile = File('${tempDir.path}/quran_source_v.pdf');
+//     await tempFile.writeAsBytes(data.buffer.asUint8List(), flush: true);
+//     setState(() {
+//       pdfPath = tempFile.path;
+//       if (pdfPath != null) {
+//         print('PDF loaded successfully: $pdfPath');
+//       } else {
+//         print('Error loading PDF');
+//       }
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: pdfPath != null
+//           ? PDFView(
+//               filePath: pdfPath!,
+//               swipeHorizontal: true,
+//               fitPolicy: FitPolicy.BOTH,
+//               onError: (error) {
+//                 print('PDF loading error: $error');
+//               },
+//               onViewCreated: (PDFViewController controller) {
+//                 print('PDF loading successful!');
+//               },
+//             )
+//           : const Center(
+//               child: CircularProgressIndicator(),
+//             ),
+//     );
+//   }
+// }
