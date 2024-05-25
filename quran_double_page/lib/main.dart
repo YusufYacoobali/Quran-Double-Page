4c10e9b35476cfcbd62699af1c61421d1773668e
Yusuf Yacoobali <yyacoobali@gmail.com>
2024-05-25 11:50:30 +0100

    vertical works

--- quran_double_page/lib/main.dart
+++ quran_double_page/lib/main.dart
@@ -54,12 +54,11 @@ class _MyPDFViewerState extends State<MyPDFViewer> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('PDF Viewer'),
-      ),
       body: pdfPath != null
           ? PDFView(
               filePath: pdfPath!,
+              swipeHorizontal: true,
+              fitPolicy: FitPolicy.BOTH,
               onError: (error) {
                 print('PDF loading error: $error');
               },
@@ -67,93 +66,9 @@ class _MyPDFViewerState extends State<MyPDFViewer> {
                 print('PDF loading successful!');
               },
             )
-          : Center(
+          : const Center(
               child: CircularProgressIndicator(),
             ),
     );
   }
 }
-
-
-
-// import 'dart:io'; // Import the 'dart:io' library for file operations
-// import 'package:flutter/material.dart';
-// import 'package:flutter_pdfview/flutter_pdfview.dart';
-
-// void main() {
-//   runApp(MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Quran PDF Viewer',
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//       ),
-//       home: HomeScreen(),
-//     );
-//   }
-// }
-
-// class HomeScreen extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Quran PDF App'),
-//       ),
-//       body: Center(
-//         child: ElevatedButton(
-//           onPressed: () {
-//             Navigator.push(
-//               context,
-//               MaterialPageRoute(
-//                 builder: (context) =>
-//                     QuranPdfViewer(pdfPath: 'assets/quran_source_v.pdf'),
-//               ),
-//             );
-//           },
-//           child: Text('Open Quran PDF'),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-// class QuranPdfViewer extends StatelessWidget {
-//   final String pdfPath;
-
-//   QuranPdfViewer({required this.pdfPath});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     print(
-//         'Checking if PDF exists at: $pdfPath'); // Print the path to check if it's correct
-//     // Check if the file exists
-//     if (File(pdfPath).existsSync()) {
-//       print(
-//           'PDF exists at the specified path.'); // Print confirmation if file exists
-//       return Scaffold(
-//         appBar: AppBar(
-//           title: Text('Quran PDF'),
-//         ),
-//         body: PDFView(
-//           filePath: pdfPath,
-//         ),
-//       );
-//     } else {
-//       print(
-//           'PDF does not exist at the specified path.'); // Print error message if file does not exist
-//       return Scaffold(
-//         appBar: AppBar(
-//           title: Text('Error'),
-//         ),
-//         body: Center(
-//           child: Text('PDF file not found.'),
-//         ),
-//       );
-//     }
-//   }
-// }
