c035c4ac0826f5941b6d1bb307176aaba480a52c
Yusuf Yacoobali <yyacoobali@gmail.com>
2024-05-29 22:37:53 +0100

    completed branch

--- quran_double_page/lib/main.dart
+++ quran_double_page/lib/main.dart
@@ -93,138 +93,131 @@ class _MyPDFViewerState extends State<MyPDFViewer> {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      behavior: HitTestBehavior.translucent, // Ensure gesture detection
-      onTap: _onScreenTap,
-      child: Scaffold(
-        body: OrientationBuilder(
-          builder: (context, orientation) {
-            bool isPortrait = orientation == Orientation.portrait;
-            if (_isPortrait != isPortrait) {
-              _isPortrait = isPortrait;
-              _orientationChanging = true;
-              _pdfViewController = null; // Reset controller to force rebuild
-            }
+    return Scaffold(
+      body: OrientationBuilder(
+        builder: (context, orientation) {
+          bool isPortrait = orientation == Orientation.portrait;
+          if (_isPortrait != isPortrait) {
+            _isPortrait = isPortrait;
+            _orientationChanging = true;
+            _pdfViewController = null; // Reset controller to force rebuild
+          }
 
-            return FutureBuilder<Map<String, String>>(
-              future: pdfPathsFuture,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                } else if (snapshot.hasError) {
-                  return Center(
-                    child: Text('Error loading PDF: ${snapshot.error}'),
-                  );
-                } else {
-                  final String pdfPath = isPortrait
-                      ? snapshot.data!['portrait']!
-                      : snapshot.data!['landscape']!;
-                  print('Displaying PDF: $pdfPath');
+          return FutureBuilder<Map<String, String>>(
+            future: pdfPathsFuture,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (snapshot.hasError) {
+                return Center(
+                  child: Text('Error loading PDF: ${snapshot.error}'),
+                );
+              } else {
+                final String pdfPath = isPortrait
+                    ? snapshot.data!['portrait']!
+                    : snapshot.data!['landscape']!;
+                print('Displaying PDF: $pdfPath');
 
-                  return Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      PDFView(
-                        key: ValueKey(pdfPath), // Force reload by changing key
-                        filePath: pdfPath,
-                        swipeHorizontal: true,
-                        fitPolicy: FitPolicy.BOTH,
-                        onRender: (pages) {
-                          setState(() {
-                            _totalPages = pages!;
-                            // Restore the current page after rendering
-                            if (_orientationChanging) {
-                              _pdfViewController?.setPage(_isPortrait
-                                  ? (_currentPage * 2).toInt()
-                                  : (_currentPage / 2).toInt());
-                            } else {
-                              _pdfViewController
-                                  ?.setPage((_currentPage / 2).toInt());
-                            }
-                            _orientationChanging = false; // Reset the flag
-                          });
-                          print(
-                              'onRender: Orientation: ${_isPortrait ? 'Portrait' : 'Landscape'}, Current Page: $_currentPage');
-                        },
-                        onViewCreated: (PDFViewController controller) {
-                          _pdfViewController = controller;
-                          // Restore the current page when view is created
-                          _pdfViewController?.setPage(_currentPage);
-                          print(
-                              'onViewCreated: Orientation: ${_isPortrait ? 'Portrait' : 'Landscape'}, Current Page: $_currentPage');
-                        },
-                        onPageChanged: (page, total) {
+                return Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    PDFView(
+                      key: ValueKey(pdfPath), // Force reload by changing key
+                      filePath: pdfPath,
+                      swipeHorizontal: true,
+                      fitPolicy: FitPolicy.BOTH,
+                      onRender: (pages) {
+                        setState(() {
+                          _totalPages = pages!;
+                          // Restore the current page after rendering
                           if (_orientationChanging) {
-                            return; // Skip updating if orientation is changing
+                            _pdfViewController?.setPage(_isPortrait
+                                ? (_currentPage * 2).toInt()
+                                : (_currentPage / 2).toInt());
+                          } else {
+                            _pdfViewController
+                                ?.setPage((_currentPage / 2).toInt());
                           }
-                          setState(() {
-                            print(
-                                'onPageChanged: Current page changing from $_currentPage');
-                            _currentPage = page!;
-                            print(
-                                'onPageChanged: Current page changed to $_currentPage');
-                          });
+                          _orientationChanging = false; // Reset the flag
+                        });
+                        print(
+                            'onRender: Orientation: ${_isPortrait ? 'Portrait' : 'Landscape'}, Current Page: $_currentPage');
+                      },
+                      onViewCreated: (PDFViewController controller) {
+                        _pdfViewController = controller;
+                        // Restore the current page when view is created
+                        _pdfViewController?.setPage(_currentPage);
+                        print(
+                            'onViewCreated: Orientation: ${_isPortrait ? 'Portrait' : 'Landscape'}, Current Page: $_currentPage');
+                      },
+                      onPageChanged: (page, total) {
+                        if (_orientationChanging) {
+                          return; // Skip updating if orientation is changing
+                        }
+                        setState(() {
                           print(
-                              'onPageChanged: Orientation: ${_isPortrait ? 'Portrait' : 'Landscape'}, Current Page: $_currentPage');
-                        },
-                        onError: (error) {
-                          print('PDF loading error: $error');
-                        },
-                        onPageError: (page, error) {
-                          print('Error on page $page: $error');
-                        },
+                              'onPageChanged: Current page changing from $_currentPage');
+                          _currentPage = page!;
+                          print(
+                              'onPageChanged: Current page changed to $_currentPage');
+                        });
+                        print(
+                            'onPageChanged: Orientation: ${_isPortrait ? 'Portrait' : 'Landscape'}, Current Page: $_currentPage');
+                      },
+                      onError: (error) {
+                        print('PDF loading error: $error');
+                      },
+                      onPageError: (page, error) {
+                        print('Error on page $page: $error');
+                      },
+                    ),
+                    if (_totalPages > 0 && _isScrollbarVisible)
+                      Positioned(
+                        bottom: 10,
+                        left: 10,
+                        right: 10,
+                        child: Row(
+                          children: [
+                            Text(
+                              '${MediaQuery.of(context).orientation == Orientation.landscape ? ((_totalPages - _currentPage) * 2 - 1) : (_totalPages - _currentPage)}/${MediaQuery.of(context).orientation == Orientation.landscape ? (_totalPages * 2 - 1) : _totalPages}',
+                              style: TextStyle(color: Colors.black),
+                            ),
+                            Expanded(
+                              child: Slider(
+                                value: _currentPage.toDouble(),
+                                min: 0,
+                                max: (_totalPages - 1).toDouble(),
+                                onChanged: (value) async {
+                                  final int pageNumber = value.toInt();
+                                  await _pdfViewController?.setPage(pageNumber);
+                                  setState(() {
+                                    _currentPage = pageNumber;
+                                    print(
+                                        'Slider onChanged: Orientation: ${_isPortrait ? 'Portrait' : 'Landscape'}, Current Page: $_currentPage');
+                                  });
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                      if (_totalPages > 0 && _isScrollbarVisible)
-                        Positioned(
-                          bottom: 10,
-                          left: 10,
-                          right: 10,
-                          child: Row(
-                            children: [
-                              Text(
-                                '${MediaQuery.of(context).orientation == Orientation.landscape ? ((_totalPages - _currentPage) * 2 - 1) : (_totalPages - _currentPage)}/${MediaQuery.of(context).orientation == Orientation.landscape ? (_totalPages * 2 - 1) : _totalPages}',
-                                style: TextStyle(color: Colors.black),
-                              ),
-                              Expanded(
-                                child: Slider(
-                                  value: _currentPage.toDouble(),
-                                  min: 0,
-                                  max: (_totalPages - 1).toDouble(),
-                                  onChanged: (value) async {
-                                    final int pageNumber = value.toInt();
-                                    await _pdfViewController
-                                        ?.setPage(pageNumber);
-                                    setState(() {
-                                      _currentPage = pageNumber;
-                                      print(
-                                          'Slider onChanged: Orientation: ${_isPortrait ? 'Portrait' : 'Landscape'}, Current Page: $_currentPage');
-                                    });
-                                  },
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      GestureDetector(
-                        behavior: HitTestBehavior
-                            .translucent, //to listen for tap events on an empty container
-
-                        onTap: _onScreenTap,
-
-                        child: Container(
-                          width: MediaQuery.of(context).size.width,
-                          height: MediaQuery.of(context).size.height,
-                        ),
-                      )
-                    ],
-                  );
-                }
-              },
-            );
-          },
-        ),
+                    GestureDetector(
+                      behavior: HitTestBehavior
+                          .translucent, //to listen for tap events on an empty container
+                      onTap: _onScreenTap,
+                      child: Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).size.height,
+                      ),
+                    )
+                  ],
+                );
+              }
+            },
+          );
+        },
       ),
     );
   }
